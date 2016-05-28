21f7a8ee87d71af1b8bdc018dcf9265f51498ebc
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2016-05-28 12:28:30 +0200

    docker: Use same container name as previous server implementation

--- docker/container-settings.sh
+++ docker/container-settings.sh
@@ -3,10 +3,10 @@
 MACHINE_TYPE=`uname -m`
 if [ x${MACHINE_TYPE} = xarmv7l ]; then
   # for ARM machines, use a different tag
-  export SERVICEREGISTRY_CONTAINER_TAG=eistec/arrowhead-servicedirectory:armhf
+  export SERVICEREGISTRY_CONTAINER_TAG=eistec/arrowhead-service-registry:armhf
 else
   # default to x86
-  export SERVICEREGISTRY_CONTAINER_TAG=eistec/arrowhead-servicedirectory:latest
+  export SERVICEREGISTRY_CONTAINER_TAG=eistec/arrowhead-service-registry:latest
 fi
 
-export SERVICEREGISTRY_DATA_CONTAINER_NAME=arrowhead-servicedirectory-store
+export SERVICEREGISTRY_DATA_CONTAINER_NAME=arrowhead-service-registry-store

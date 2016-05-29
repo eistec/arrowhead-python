1ca9dd466807f0e6ed412a4042bea302106dee5d
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2016-05-29 14:44:03 +0200

    arrowhead/services: Add docs, fix pylint issues

--- arrowhead/services.py
+++ arrowhead/services.py
@@ -1,19 +1,22 @@
-have_json = True
+"""Functions for serialization/deserialization of Arrowhead services"""
 try:
     import json
 except ImportError:
     try:
         import simplejson as json
     except ImportError:
-        have_json = False
+        HAVE_JSON = False
+else:
+    HAVE_JSON = True
 
-have_xml = True
 try:
     import xml.etree.ElementTree as ET
 except ImportError:
-    have_xml = False
+    HAVE_XML = False
+else:
+    HAVE_XML = True
 
-service_attributes = ("name", "type", "host", "port", "domain")
+SERVICE_ATTRIBUTES = ("name", "type", "host", "port", "domain")
 
 __all__ = [
     'ServiceError',
@@ -31,51 +34,75 @@ __all__ = [
 
 
 class ServiceError(Exception):
-    pass
+    """Exception raised by run time errors in this module"""
 
 
 def service_dict(**kwargs):
-    '''Create a new service dict'''
-    res = {key: kwargs.get(key, None) for key in service_attributes}
+    """Create a new service dict"""
+    res = {key: kwargs.get(key, None) for key in SERVICE_ATTRIBUTES}
     res['properties'] = kwargs.get('properties', {})
     return res
 
-if have_json:
+if HAVE_JSON:
     def service_from_json_dict(js_dict):
-        d = js_dict
-        attrs = {key: d.get(key, None) for key in service_attributes}
+        """Create a service dict from a dict of the JSON representation
+
+        This is suitable for passing already parsed JSON dicts to.
+
+        :param js_dict: JSON dict
+        :type js_dict: dict
+        """
+        attrs = {key: js_dict.get(key, None) for key in SERVICE_ATTRIBUTES}
         props = {}
-        jp = d.get('properties', {'property': []})['property']
-        for p in jp:
+        js_props = js_dict.get('properties', {'property': []})['property']
+        for jprop in js_props:
             try:
-                props[p['name']] = p['value']
+                props[jprop['name']] = jprop['value']
             except KeyError:
                 continue
         return service_dict(properties=props, **attrs)
 
     def service_from_json(jsonstr):
+        """Create a service dict from a JSON string representation of the service
+
+        The JSON string can be obtained from :meth:`arrowhead.services.service_to_json`
+
+        :param jsonstr: JSON string representation of a service
+        :type jsonstr: string
+        """
         return service_from_json_dict(json.loads(jsonstr))
 
     def service_to_json_dict(service):
-        '''Convert a service dict to a JSON representation dict'''
+        """Convert a service dict to a JSON representation dict"""
         props = [{'name': name, 'value': value}
                  for name, value in service['properties'].items()]
-        res = {key: service.get(key, None) for key in service_attributes}
+        res = {key: service.get(key, None) for key in SERVICE_ATTRIBUTES}
         res['properties'] = {"property": props}
         return res
 
     def service_to_json(service):
-        '''Convert a JSON service dict to JSON text'''
+        """Convert a JSON service dict to JSON text
+
+        :param service: Service to convert
+        :type service: dict
+        :returns: The service encoded as an JSON string
+        :rtype: string
+        """
         return json.dumps(service_to_json_dict(service))
 
 
 def service_to_xml(service):
-    '''Convert a service dict to an XML representation'''
-    # note: Does not use any xml functions, hence unaffected by have_xml
+    """Convert a service dict to an XML representation
+
+    :param service: Service to convert
+    :type service: dict
+    :returns: The service encoded as an XML string
+    :rtype: string
+    """
+    # note: Does not use any xml functions, hence unaffected by HAVE_XML
     props = ''.join(
-        ('<property><name>%s</name><value>%s</value></property>' %
-         (k, v)) for (
-            k, v) in service['properties'].items())
+        ('<property><name>%s</name><value>%s</value></property>' % (k, v))
+        for (k, v) in service['properties'].items())
     xml_str = (
         '<service>'
         '<domain>%s</domain>'
@@ -90,41 +117,41 @@ def service_to_xml(service):
                          props)
     return xml_str
 
-if have_xml:
+if HAVE_XML:
     def service_from_xml(xmlstr):
-        '''Convert XML representation of service to service dict'''
+        """Convert XML representation of service to service dict"""
         res = service_dict()
         root = ET.fromstring(xmlstr)
-        for child in root:
-            if child.tag in res:
+        for node in root:
+            if node.tag in res:
                 # disallow multiple occurrences of the same tag
                 raise ServiceError(
                     "Multiple occurrence of tag <%s>" %
-                    child.tag, child.tag, child.text)
-            if child.tag in service_attributes:
-                res[child.tag] = child.text.strip()
+                    node.tag, node.tag, node.text)
+            if node.tag in SERVICE_ATTRIBUTES:
+                res[node.tag] = node.text.strip()
                 # ignoring any XML attributes or child nodes
-            elif child.tag == 'properties':
+            elif node.tag == 'properties':
                 props = {}
-                for c in child:
-                    if c.tag != 'property':
+                for child_node in node:
+                    if child_node.tag != 'property':
                         # ignoring any unknown tags
                         continue
                     name = None
                     value = None
-                    for pc in c:
-                        if pc.tag == 'name':
+                    for child_prop in child_node:
+                        if child_prop.tag == 'name':
                             if name is not None:
                                 raise ServiceError(
                                     "Multiple occurrence of property tag <%s>" %
-                                    pc.tag, pc.tag, pc.text)
-                            name = pc.text.strip()
-                        elif pc.tag == 'value':
+                                    child_prop.tag, child_prop.tag, child_prop.text)
+                            name = child_prop.text.strip()
+                        elif child_prop.tag == 'value':
                             if value is not None:
                                 raise ServiceError(
                                     "Multiple occurrence of property tag <%s>" %
-                                    pc.tag, pc.tag, pc.text)
-                            value = pc.text.strip()
+                                    child_prop.tag, child_prop.tag, child_prop.text)
+                            value = child_prop.text.strip()
                         # ignoring any unknown tags
                     if not name:
                         raise ServiceError(
@@ -140,7 +167,11 @@ if have_xml:
 
 
 def service_to_corelf(service):
-    '''Convert a service dict to CoRE Link-format'''
+    """Convert a service dict to CoRE Link-format (:rfc:`6690`)
+
+    :param service: Service to convert
+    :type service: dict
+    """
     host = str(service['host'])
     if ':' in host:
         # assume IPv6 address, wrap in brackets for URL construction
@@ -156,14 +187,37 @@ def service_to_corelf(service):
 
 
 def servicelist_to_json(slist):
+    """Convert a list of service dicts to a JSON string
+
+    :param slist: List of services to convert
+    :type slist: iterable
+    :returns: The service list encoded as a JSON string
+    :rtype: string
+    """
     return json.dumps({'service': [service_to_json_dict(s) for s in slist]})
 
 
 def servicelist_to_xml(slist):
+    """Convert a list of service dicts to an XML string
+
+    :param slist: List of services to convert
+    :type slist: iterable
+    :returns: The service list encoded as an XML string
+    :rtype: string
+    """
+
     return '<serviceList>' + \
         ''.join([service_to_xml(s) for s in slist]) + '</serviceList>'
 
 
 def servicelist_to_corelf(slist, uri_base):
-    '''Return a core link-format string for the given services'''
+    """Convert a list of services to a CoRE Link-format (:rfc:`6690`) string
+
+    :param slist: List of services to convert
+    :type slist: iterable
+    :param uri_base: Base URI for the links
+    :type uri_base: string
+    :returns: The service list encoded as an application/link-format string
+    :rtype: string
+    """
     return ','.join(['<%s/%s>' % (uri_base, s['name']) for s in slist])

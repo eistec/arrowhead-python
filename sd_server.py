eee10cc45654cd37721b901b6bc53b21a7023edf
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2016-05-30 07:47:54 +0200

    sd_server: Clean up bind address code

--- sd_server.py
+++ sd_server.py
@@ -4,9 +4,7 @@ import logging
 import argparse
 
 import asyncio
-
-import aiocoap.resource as resource
-import aiocoap
+import ipaddress
 
 from arrowhead.servicedirectory import ServiceDirectory
 from arrowhead.servicedirectory import coap
@@ -16,16 +14,18 @@ loglevels = [logging.CRITICAL, logging.ERROR, logging.WARN, logging.INFO, loggin
 
 def main(argv=None):
     parser = argparse.ArgumentParser(argv)
-    parser.add_argument('-f', '--dbfile', type=str, metavar='FILE', default='directory_db.json',
+    parser.add_argument('-f', '--dbfile', type=str, metavar='FILE', default='directory_db',
                         help="Service directory database filename")
     parser.add_argument("-v", "--verbosity", type=int, default=4,
                         help="set logging verbosity, 1=CRITICAL, 5=DEBUG")
-    parser.add_argument("--httpport", type=int, default=8080,
-                        help="HTTP port, use 0 to disable")
-    parser.add_argument("--http-host", default='::',
-                        help="HTTP server bind address")
-    parser.add_argument("--coapport", type=int, default=5683,
+    parser.add_argument("--coap-port", type=int, default=5683,
                         help="CoAP port, use 0 to disable")
+    parser.add_argument("--coap-bind", default='::',
+                        help="CoAP server bind address")
+    parser.add_argument("--http-port", type=int, default=8080,
+                        help="HTTP port, use 0 to disable")
+    parser.add_argument("--http-bind", default='::',
+                        help="HTTP server bind address")
     args = parser.parse_args()
 
     # logging setup
@@ -38,16 +38,23 @@ def main(argv=None):
 
     loop = asyncio.get_event_loop()
 
-    if args.coapport:
+    if args.coap_port:
+        # aiocoap only supports IPv6 sockets, use ::ffff:123.45.67.89 for
+        # listening on IPv4 addresses
+        coap_addr = ipaddress.ip_address(args.coap_bind)
+        if isinstance(coap_addr, ipaddress.IPv4Address):
+            coap_bind = '::ffff:' + str(coap_addr)
+        else:
+            coap_bind = str(coap_addr)
         # TODO: Update this when aiocoap 0.3 is released on PyPi (loop support)
         #coap_server = coap.Server(directory=directory, loop=loop)
-        coap_server = coap.Server(directory=directory)
+        coap_server = coap.Server(directory=directory, bind=(coap_bind, args.coap_port))
         asyncio.async(coap_server.context)
-    if args.httpport:
+    if args.http_port:
         http_directory = http.Server(directory=directory, loop=loop)
         http_handler = http_directory.make_handler()
-        http_server = loop.create_server(http_handler, host=args.http_host,
-            port=args.httpport)
+        http_server = loop.create_server(http_handler, host=args.http_bind,
+            port=args.http_port)
         asyncio.async(http_server)
 
     asyncio.get_event_loop().run_forever()

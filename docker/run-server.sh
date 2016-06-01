08446cec132bacc7d7a47126c6ff2a2a3383cd23
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2016-06-01 13:25:08 +0200

    docker: Port map HTTP as well

--- docker/run-server.sh
+++ docker/run-server.sh
@@ -4,12 +4,7 @@
 
 # This will start an ephemeral container for the Arrowhead service registry.
 # The data will be linked from the data container
-# Only the HTTP and CoAP frontends will be exposed, the DNS-SD service is only
-# available within the container
-#
-# To expose the DNS service as well, add:
-# -p 53:53/udp -p 53:53
-# to the other flags below.
 docker run --rm --volumes-from "${SERVICEREGISTRY_DATA_CONTAINER_NAME}" \
+    -p 8045:8045 \
     -p 5683:5683/udp \
     "${SERVICEREGISTRY_CONTAINER_TAG}"

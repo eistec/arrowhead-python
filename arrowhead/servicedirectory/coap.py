8fe17733109dddd9348cb2ea01ee8e754008d7b3
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2016-06-06 08:02:00 +0200

    coap: Refactor resource dispatching

--- arrowhead/servicedirectory/coap.py
+++ arrowhead/servicedirectory/coap.py
@@ -1,22 +1,7 @@
 """CoAP implementation of the Arrowhead Service Directory based around aiocoap"""
 
+import re
 import asyncio
-try:
-    import json
-except ImportError:
-    try:
-        import simplejson as json
-    except ImportError:
-        HAVE_JSON = False
-else:
-    HAVE_JSON = True
-
-try:
-    import xml.etree.ElementTree as ET
-except ImportError:
-    HAVE_XML = False
-else:
-    HAVE_XML = True
 
 import aiocoap.resource as resource
 from aiocoap.numbers import media_types_rev
@@ -27,18 +12,15 @@ from .. import LogMixin
 
 from .. import services
 
-__all__ = [
-    'ParentSite',
-    'ServiceResource',
-    'TypeResource',
-    'PublishResource',
-    'UnpublishResource']
+__all__ = ['ServiceDirectoryCoAP']
 
 URI_PATH_SEPARATOR = '/'
 
 class RequestDispatcher(object):
     """Helper functions for dispatching requests based on their Accept or
     Content-format header options"""
+
+    default_content_type = None
 
     @staticmethod
     def dispatch_input(request, handlers, *args, **kwargs):
@@ -84,77 +66,182 @@ class BadRequestError(aiocoap.error.RenderableError):
     code = Code.BAD_REQUEST
     message = "BadRequest"
 
-class ParentSite(LogMixin, resource.Site):
-    """CoAP Site resource which sends the request to the closest parent if the
-    exact resource specified in the URL was not found"""
+class Site(LogMixin, resource.Site):
+    """CoAP Site resource with path regex matching"""
 
     @asyncio.coroutine
     def render(self, request):
         """Dispatch rendering to the best matching resource"""
         path = tuple(request.opt.uri_path)
-        for i in range(len(path), 0, -1):
-            key = request.opt.uri_path[:i]
-            try:
-                child = self._resources[key]
-            except KeyError:
-                continue
-            request.prepath = key
-            request.postpath = request.opt.uri_path[i:]
-            self.log.debug(
-                'prepath: %r postpath: %r' %
-                (request.prepath, request.postpath))
-            self.log.debug('Request: %r' % (request, ))
-            self.log.debug('options: %r' % (request.opt, ))
-            return child.render(request)
-        raise aiocoap.error.NoResource()
+        # Only compare against resources with the same number of path components
+        matches = [(key, res) for key, res in self._resources.items() if len(key) == len(path)]
+        for level in range(len(path)):
+            # Filter routes matching the path, one level at a time
+            matches = [(key, res) for key, res in matches if re.fullmatch(key[level], path[level])]
+        if len(matches) == 0:
+            raise aiocoap.error.NoResource()
+        elif len(matches) > 1:
+            raise aiocoap.error.RenderableError(
+                "Ambiguous matches: {} = {}".format(repr(path), repr(matches)))
+        self.log.info("{} -> {}".format(repr(path), repr(matches[0][0])))
+        return matches[0][1].render(request)
 
-
-class ServiceResource(RequestDispatcher, LogMixin, resource.ObservableResource):
-    """/service resource"""
+class ServiceDirectoryCoAP(RequestDispatcher, Site):
+    """Service Directory resource handler class"""
     service_url = '/service'
+    type_url = '/type'
+
     default_content_type = media_types_rev['application/json']
-    service_handlers = {
+
+    service_output_handlers = {
         media_types_rev['application/json']: services.service_to_json,
         media_types_rev['application/link-format']: services.service_to_corelf,
         media_types_rev['application/xml']: services.service_to_xml,
     }
 
-    def __init__(self, directory, *args, **kwargs):
+    service_input_handlers = {
+        media_types_rev['application/json']: services.service_from_json,
+        media_types_rev['application/xml']: services.service_from_xml,
+    }
+
+    class Resource(resource.Resource):
+        """Generic resource class"""
+        def __init__(self, *args, get=None, put=None, post=None, delete=None, **kwargs):
+            """Constructor
+
+            :param get: callback for GET requests
+            :type get: asyncio.coroutine or None
+            :param put: callback for PUT requests
+            :type put: asyncio.coroutine or None
+            :param post: callback for POST requests
+            :type post: asyncio.coroutine or None
+            :param delete: callback for DELETE requests
+            :type delete: asyncio.coroutine or None
+            """
+            super().__init__(*args, **kwargs)
+            if get is not None:
+                self.render_get = get
+            if put is not None:
+                self.render_put = put
+            if post is not None:
+                self.render_post = post
+            if delete is not None:
+                self.render_delete = delete
+
+    class ObservableResource(Resource, resource.ObservableResource):
+        """Generic observable resource class"""
+
+    def __init__(self, directory, uri_prefix, *args, **kwargs):
         """Constructor
 
         :param directory: Service directory backend
         :type directory: arrowhead.servicedirectory.directory.ServiceDirectory
+        :param uri_prefix: URI prefix for this site
+        :type uri_prefix: tuple(strings...)
         """
         super().__init__(*args, **kwargs)
+        self.uri_prefix = uri_prefix
+
         self.slist_handlers = {
-            media_types_rev['application/json']: \
-                lambda slist, req: services.servicelist_to_json(slist),
-            media_types_rev['application/xml']: \
-                lambda slist, req: services.servicelist_to_xml(slist),
+            media_types_rev['application/json']: services.servicelist_to_json,
+            media_types_rev['application/xml']: services.servicelist_to_xml,
             media_types_rev['application/link-format']: self._slist_to_corelf,
         }
+
+        self.tlist_handlers = {
+            media_types_rev['application/json']: services.typelist_to_json,
+            #media_types_rev['application/xml']: services.typelist_to_xml,
+            media_types_rev['application/link-format']: self._tlist_to_corelf,
+        }
+        self._create_resources()
+        self.log.debug('Resources: {}'.format(repr(self._resources)))
         self._directory = directory
         self._directory.add_notify_callback(self.notify)
         self.notify()
 
-    def _slist_to_corelf(self, slist, request):
-        """Convert a service list to CoRE Link-format links to other resources"""
-        uri_base = '/' + '/'.join(request.prepath[:-1]) + self.service_url
-        return services.servicelist_to_corelf(slist, uri_base)
+    def _create_resources(self):
+        """Create all the resources of the service directory REST API"""
+        self._servicelist_resource = self.ObservableResource(get=self._render_servicelist)
+        self._service_resource = self.ObservableResource(get=self._render_service)
+        self._typelist_resource = self.ObservableResource(get=self._render_typelist)
+        self._type_resource = self.ObservableResource(get=self._render_type)
+        self.add_resource(
+            self.uri_prefix + ('service', ), self._servicelist_resource)
+        self.add_resource(
+            self.uri_prefix + ('service', '.*'), self._service_resource)
+        self.add_resource(
+            self.uri_prefix + ('type', ), self._typelist_resource)
+        self.add_resource(
+            self.uri_prefix + ('type', '.*'), self._type_resource)
+        self.add_resource(
+            self.uri_prefix + ('publish', ),
+            self.Resource(post=self._render_publish))
+        self.add_resource(
+            self.uri_prefix + ('unpublish', ),
+            self.Resource(post=self._render_unpublish))
 
     def notify(self):
         """Send notifications to all registered subscribers"""
         self.log.debug('Notifying subscribers')
-        self.updated_state()
+        self._servicelist_resource.updated_state()
+        self._service_resource.updated_state()
+        self._typelist_resource.updated_state()
+        self._type_resource.updated_state()
 
-    def _render_service(self, request, service):
-        payload = self.dispatch_output(request, self.service_handlers, service)
+    def _slist_to_corelf(self, slist):
+        """Convert a service list to CoRE Link-format links to other resources"""
+        uri_base = '/' + '/'.join(self.uri_prefix) + self.service_url
+        return services.servicelist_to_corelf(slist, uri_base)
+
+    def _tlist_to_corelf(self, tlist):
+        """Convert a type list to CoRE Link-format links to other resources"""
+        uri_base = '/' + '/'.join(self.uri_prefix) + self.type_url
+        return services.typelist_to_corelf(tlist, uri_base)
+
+    @asyncio.coroutine
+    def _render_service(self, request):
+        """GET handler, respond with a single service
+
+        :param request: The inbound CoAP request
+        :type request: aiocoap.Message
+        """
+        name = request.opt.uri_path[-1]
+        try:
+            service = self._directory.service(name=name)
+        except self._directory.DoesNotExist:
+            # Could not find a service by that name, send response code
+            raise NotFoundError()
+        payload = self.dispatch_output(request, self.service_output_handlers, service)
         msg = aiocoap.Message(code=Code.CONTENT, payload=payload.encode('utf-8'))
         msg.opt.content_format = request.opt.accept
+        if msg.opt.content_format is None:
+            msg.opt.content_format = self.default_content_type
         return msg
 
-    def _render_servicelist(self, request, slist):
-        payload = self.dispatch_output(request, self.slist_handlers, slist, request)
+    @asyncio.coroutine
+    def _render_servicelist(self, request):
+        """GET handler, respond with a list of registered services
+
+        :param request: The inbound CoAP request
+        :type request: aiocoap.Message
+        """
+        slist = self._directory.service_list()
+        payload = self.dispatch_output(request, self.slist_handlers, slist)
+        msg = aiocoap.Message(code=Code.CONTENT, payload=payload.encode('utf-8'))
+        msg.opt.content_format = request.opt.accept
+        if msg.opt.content_format is None:
+            msg.opt.content_format = self.default_content_type
+        return msg
+
+    @asyncio.coroutine
+    def _render_typelist(self, request):
+        """GET handler, respond with a list of registered service types
+
+        :param request: The inbound CoAP request
+        :type request: aiocoap.Message
+        """
+        tlist = self._directory.types()
+        payload = self.dispatch_output(request, self.tlist_handlers, tlist)
         msg = aiocoap.Message(code=Code.CONTENT, payload=payload.encode('utf-8'))
         msg.opt.content_format = request.opt.accept
         if msg.opt.content_format is None:
@@ -162,42 +249,26 @@ class ServiceResource(RequestDispatcher, LogMixin, resource.ObservableResource):
         return msg
 
     @asyncio.coroutine
-    def render_get(self, request):
-        """GET handler, respond with either a service list or a single service
+    def _render_type(self, request):
+        """GET handler, respond with a list of registered services of the given type
 
         :param request: The inbound CoAP request
         :type request: aiocoap.Message
         """
-        name = '/'.join(request.postpath)
-        self.log.debug('GET service %s' % (name, ))
-        if name:
-            try:
-                service = self._directory.service(name=name)
-            except self._directory.DoesNotExist:
-                # Could not find a service by that name, send response code
-                raise NotFoundError()
-            return self._render_service(request, service)
-        else:
-            slist = self._directory.service_list()
-            return self._render_servicelist(request, slist)
-
-
-class PublishResource(RequestDispatcher, LogMixin, resource.Resource):
-    """/publish resource"""
-    service_input_handlers = {
-        media_types_rev['application/json']: services.service_from_json,
-        media_types_rev['application/xml']: services.service_from_xml,
-    }
-
-    def __init__(self, directory, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._directory = directory
+        tname = request.opt.uri_path[-1]
+        slist = self._directory.service_list(type=tname)
+        payload = self.dispatch_output(request, self.slist_handlers, slist)
+        msg = aiocoap.Message(code=Code.CONTENT, payload=payload.encode('utf-8'))
+        msg.opt.content_format = request.opt.accept
+        if msg.opt.content_format is None:
+            msg.opt.content_format = self.default_content_type
+        return msg
 
     @asyncio.coroutine
-    def render_post(self, request):
+    def _render_publish(self, request):
         """POST handler
 
-        This method parses the received JSON data as a service and updates the
+        This method parses the received payload data as a service and updates the
         directory with the new data.
 
         :param request: The inbound CoAP request
@@ -207,7 +278,8 @@ class PublishResource(RequestDispatcher, LogMixin, resource.Resource):
         """
         self.log.debug('POST %r' % (request.opt.uri_path, ))
         try:
-            service = self.dispatch_input(request, self.service_input_handlers, request.payload.decode('utf-8'))
+            service = self.dispatch_input(
+                request, self.service_input_handlers, request.payload.decode('utf-8'))
         except services.ServiceError:
             raise BadRequestError()
         if not service['name']:
@@ -227,20 +299,8 @@ class PublishResource(RequestDispatcher, LogMixin, resource.Resource):
         msg.opt.content_format = media_types_rev['text/plain']
         return msg
 
-class UnpublishResource(RequestDispatcher, LogMixin, resource.Resource):
-    """/unpublish resource"""
-
-    name_input_handlers = {
-        media_types_rev['application/json']: services.service_from_json,
-        media_types_rev['application/xml']: services.service_from_xml,
-    }
-
-    def __init__(self, directory, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._directory = directory
-
     @asyncio.coroutine
-    def render_post(self, request):
+    def _render_unpublish(self, request):
         """POST handler
 
         This method parses the received JSON data as a service and tells the
@@ -252,11 +312,12 @@ class UnpublishResource(RequestDispatcher, LogMixin, resource.Resource):
         :rtype: aiocoap.Message
         """
         try:
-            service = self.dispatch_input(request, self.name_input_handlers, request.payload)
-        except Exception as exc:
-            # any error decoding the payload
-            print(repr(exc))
-            raise UnsupportedMediaTypeError()
+            service = self.dispatch_input(request, self.service_input_handlers, request.payload)
+        except services.ServiceError:
+            raise BadRequestError()
+        if not service['name']:
+            # bad input
+            raise BadRequestError()
         self._directory.unpublish(name=service['name'])
         payload = 'POST OK'
         code = Code.DELETED
@@ -264,61 +325,7 @@ class UnpublishResource(RequestDispatcher, LogMixin, resource.Resource):
         msg.opt.content_format = media_types_rev['text/plain']
         return msg
 
-class TypeResource(ServiceResource):
-    """/type resource"""
-    type_url = '/type'
-    tlist_handlers = {
-        media_types_rev['application/json']: services.typelist_to_json,
-        media_types_rev['application/link-format']: services.typelist_to_corelf,
-        #media_types_rev['application/xml']: services.typelist_to_xml,
-    }
-
-    def __init__(self, *args, **kwargs):
-        """Constructor"""
-        super().__init__(*args, **kwargs)
-        self.tlist_handlers = {
-            media_types_rev['application/json']: \
-                lambda tlist, req: services.typelist_to_json(tlist),
-            #media_types_rev['application/xml']: \
-            #    lambda tlist, req: services.typelist_to_xml(slist),
-            media_types_rev['application/link-format']: self._tlist_to_corelf,
-        }
-
-    def _tlist_to_corelf(self, tlist, request):
-        """Convert a type list to CoRE Link-format links to other resources"""
-        uri_base = '/' + '/'.join(request.prepath[:-1]) + self.service_url
-        return services.typelist_to_corelf(tlist, uri_base)
-
-    def _render_typelist(self, request, tlist):
-        payload = self.dispatch_output(request, self.tlist_handlers, tlist, request)
-        msg = aiocoap.Message(code=Code.CONTENT, payload=payload.encode('utf-8'))
-        msg.opt.content_format = request.opt.accept
-        if msg.opt.content_format is None:
-            msg.opt.content_format = media_types_rev['application/json']
-        return msg
-
-    @asyncio.coroutine
-    def render_get(self, request):
-        """GET handler, return a list of types
-
-        :param request: The inbound CoAP request
-        :type request: aiocoap.Message
-        :return: A CoAP response
-        :rtype: aiocoap.Message
-        """
-        name = '/'.join(request.postpath)
-        if name:
-            slist = self._directory.service_list(type=name)
-            if not slist:
-                # Could not find a service by that name, send response code
-                raise NotFoundError()
-            return self._render_servicelist(request, slist)
-        else:
-            tlist = self._directory.types()
-            return self._render_typelist(request, tlist)
-
-
-class Server(ParentSite):
+class Server(object):
     """CoAP server implementation"""
 
     def __init__(self, *, directory, **kwargs):
@@ -329,22 +336,9 @@ class Server(ParentSite):
         """
         super().__init__()
         self._directory = directory
-        self.site = ParentSite()
+        self.site = ServiceDirectoryCoAP(directory=directory, uri_prefix=('servicediscovery', ))
         self.context = aiocoap.Context.create_server_context(self.site, **kwargs)
 
         self.site.add_resource(
             ('.well-known', 'core'),
             resource.WKCResource(self.site.get_resources_as_linkheader))
-
-        self.site.add_resource(
-            ('servicediscovery', 'service'),
-            ServiceResource(directory=directory))
-        self.site.add_resource(
-            ('servicediscovery', 'publish'),
-            PublishResource(directory=directory))
-        self.site.add_resource(
-            ('servicediscovery', 'unpublish'),
-            UnpublishResource(directory=directory))
-        self.site.add_resource(
-            ('servicediscovery', 'type'),
-            TypeResource(directory=directory))
